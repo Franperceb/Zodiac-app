a4327e4a3d2c05abdad604fcd2e0a01edc63709c
Franperceb <42518428+Franperceb@users.noreply.github.com>
2022-11-20 23:35:06 -0600

    First Android Project. Zodiac App

---
+++ 9no semestre/App21102022/app/src/main/java/com/example/app21102022/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.app21102022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
